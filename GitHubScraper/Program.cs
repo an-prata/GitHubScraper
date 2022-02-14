37ab7c3cd53a0e16194789896469112b2b3a0d6b
an-prata <evanrileyoverman@gmail.com>
2022-02-13 21:14:49 -0800

    fixed dumb args mistake and added file writing

--- GitHubScraper/Program.cs
+++ GitHubScraper/Program.cs
@@ -1,4 +1,5 @@
-﻿using static GitHubScraper.ReadMeScraper;
+﻿using static System.Text.Encoding;
+using static GitHubScraper.ReadMeScraper;
 
 namespace GitHubScraper;
 
@@ -6,7 +7,24 @@ public class Program
 {
 	public static void Main(string[] args)
 	{
-		Console.WriteLine($"README from GitHub repo {args[1]}:\n");
-		Console.WriteLine(ScrapeRaw(args[1]));
+		Console.WriteLine($"README from GitHub repo {args[0]}:\n");
+
+		string scrape = ScrapeRaw(args[0]);
+		Console.WriteLine(scrape);
+
+		try
+		{
+			FileStream file = File.Open(args[1], FileMode.Create);
+			file.Write(UTF8.GetBytes(scrape));
+			file.Close();
+		}
+		catch(IndexOutOfRangeException)
+		{
+			return;
+		}
+		catch(FileNotFoundException)
+		{
+			Console.Write($"File {args[1]} was not found.");
+		}
 	}
 }
